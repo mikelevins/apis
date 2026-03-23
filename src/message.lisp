aea68558a5d6008dd1fc4ad8836a21bb2d92eae4
mikel evins <mevins@me.com>
2026-03-22 21:52:21 -0500

    implement message

--- src/message.lisp
+++ src/message.lisp
@@ -15,46 +15,39 @@
 ;;; ---------------------------------------------------------------------
 
 (defclass message ()
-  (;; a vector of 20 bytes
-   (id :reader message-id :initform (makeid) :initarg :id :type integer)
-   ;; a worker
-   (from :reader message-from :initform nil :initarg :from)
-   ;; a worker
-   (to :reader message-to :initform nil :initarg :to)
-   ;; a keyword
-   (operation :reader message-operation :initform :ping :initarg :operation :type (or null symbol))
+  ((id :reader message-id :initform (makeid) :initarg :id :type integer)
+   ;; a ULID identifying the sender, or nil
+   (from :reader message-from :initform nil :initarg :from :type (or integer null))
+   ;; a ULID identifying the recipient
+   (to :reader message-to :initform nil :initarg :to :type (or integer null))
+   ;; a keyword naming the operation
+   (operation :reader message-operation :initform :ping :initarg :operation :type symbol)
    ;; a plist
    (data :reader message-data :initform nil :initarg :data)
    (timestamp :reader message-timestamp :initform (get-universal-time) :initarg :timestamp :type integer)
    (time-to-live :reader message-time-to-live
-                 :initform *default-message-time-to-live* :initarg :time-to-live :type integer)))
+                 :initform *default-message-time-to-live* :initarg :time-to-live :type integer)
+   ;; a ULID identifying the message that caused this one, or nil
+   (cause :reader message-cause :initform nil :initarg :cause :type (or integer null))))
 
 (defmethod print-object ((obj message) out-stream)
   (print-unreadable-object (obj out-stream :type nil :identity nil)
-    (format out-stream "message ~X from: ~S to: ~S ~S ~S"
-            (type-of obj)
-            (type-of (message-from obj))
-            (type-of (message-to obj))
+    (format out-stream "message ~A from: ~A to: ~A ~S ~S"
+            (format-id (message-id obj))
+            (if (message-from obj) (format-id (message-from obj)) "nil")
+            (if (message-to obj) (format-id (message-to obj)) "nil")
             (message-operation obj)
             (message-data obj))))
 
 (defun message (&key (id (makeid)) from to (operation :ping) data
-                  (timestamp (get-universal-time))(time-to-live *default-message-time-to-live*))
+                  (timestamp (get-universal-time))
+                  (time-to-live *default-message-time-to-live*)
+                  cause)
   (make-instance 'message :id id :from from :to to :operation operation :data data
-                          :timestamp timestamp :time-to-live time-to-live))
+                          :timestamp timestamp :time-to-live time-to-live
+                          :cause cause))
 
-
-(defparameter *dead-messages* (make-array 32 :initial-element nil :fill-pointer 0 :adjustable t))
-(defmethod file-dead-message ((message message) &key (explanation "Message delivery failed."))
-  (let ((dm (cons explanation message)))
-    (format t "~A: ~S" explanation message)
-    (vector-push-extend dm *dead-messages* 16)))
-
-(defmethod send ((msg message))
-  (let ((to (message-to msg)))
-    (if to
-        (let ((q (message-queue to)))
-          (bt:with-recursive-lock-held ((queues::lock-of q))
-            (queues:qpush q msg)
-            (bt:signal-semaphore (message-semaphore to))))
-        (file-dead-message msg))))
+#+repl (defvar *msg* (apis:message :from *id1* :to *id2* :operation :greet :data '(:name "Alice")))
+#+repl (describe *msg*)
+#+repl (defvar *reply* (apis:message :to *id1* :operation :ack :cause (apis:message-id *msg*)))
+#+repl (describe *reply*)
